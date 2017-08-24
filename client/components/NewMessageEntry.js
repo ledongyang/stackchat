5e40fa5489a42a60d7deeff515492bcbc4632ee1
Michael Yang <ledongyang@gmail.com>
2017-08-24 13:49:39 -0400

    finish 5

--- client/components/NewMessageEntry.js
+++ client/components/NewMessageEntry.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import store, { writeMessage, gotNewMessageFromServer } from '../store';
+import axios from 'axios';
+import socket from '../socket';
 
 export default class NewMessageEntry extends Component {
+  constructor() {
+    super()
+    this.state = store.getState()
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromStore = store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromStore();
+  }
+
+  handleChange(e){
+    store.dispatch(writeMessage(e.target.value))
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    axios.post('/api/messages', { content: this.state.newMessageEntry,
+      channelId: this.props.channelId
+     })
+     .then(res => res.data)
+     .then(message => {
+       console.log(message)
+       store.dispatch(gotNewMessageFromServer(message));
+       socket.emit('new-message', message);
+      })
+      store.dispatch(writeMessage(''));
+  }
 
   render () {
+
     return (
-      <form id="new-message-form">
+      <form id="new-message-form" onSubmit={this.handleSubmit}>
         <div className="input-group input-group-lg">
           <input
             className="form-control"
             type="text"
             name="content"
+            value={this.state.newMessageEntry}
             placeholder="Say something nice..."
+            onChange={this.handleChange}
           />
           <span className="input-group-btn">
             <button className="btn btn-default" type="submit">Chat!</button>
--- client/store.js
+++ client/store.js
@@ -1,21 +1,53 @@
-const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+import { createStore } from 'redux';
 
-const initialState = {
-  messages: []
+// ACTION TYPES
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const WRITE_MESSAGE = 'WRITE_MESSAGE';
+const GOT_NEW_MESSAGE_FROM_SERVER = 'GOT_NEW_MESSAGE_FROM_SERVER';
+
+// ACTION CREATORS
+export function gotMessagesFromServer (messages) {
+  return {
+    type: GOT_MESSAGES_FROM_SERVER,
+    messages
+  };
 }
 
-function reducer (state = initialState, action) {
-  switch (action.type){
-    case GOT_MESSAGES_FROM_SERVER:
-      return Object.assign({}, state, {message: action.message});
-    default:
-      return state;
+export function writeMessage(inputContent) {
+  return {
+    type: WRITE_MESSAGE,
+    newMessageEntry: inputContent
   }
 }
 
-export const gotMessagesFromServer = function (messages) {
+export function gotNewMessageFromServer(message) {
   return {
-    type: GOT_MESSAGES_FROM_SERVER, // be sure to use the constant, not a string literal
-    messages: messages
-  };
+    type: GOT_NEW_MESSAGE_FROM_SERVER,
+    message: message
+  }
+}
+
+// INITIAL STATE
+const initialState = {
+  messages: [],
+  newMessageEntry: ''
 };
+
+// REDUCER
+function reducer (state = initialState, action) {
+  switch (action.type) {
+    case GOT_MESSAGES_FROM_SERVER:
+       return Object.assign({}, state, { messages: action.messages });
+    case WRITE_MESSAGE:
+       return Object.assign({}, state, { newMessageEntry: action.newMessageEntry });
+    case GOT_NEW_MESSAGE_FROM_SERVER:
+       return Object.assign({}, state, { messages: state.messages.concat(action.message) });
+    default:
+       return state;
+  }
+}
+
+// STORE
+const store = createStore(reducer);
+export default store;
+
